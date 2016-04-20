4d4127ae4227c9dbcf232748d95ec3aaf183b269
Joel Frederico <joelfred@slac.stanford.edu>
2016-04-19 18:31:48 -0700

    Script to use bumpversion!

---
+++ release.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import subprocess
+import shlex
+import argparse
+
+
+def runcommand(command, **kwargs):
+    subprocess.run(shlex.split(command), **kwargs)
+
+
+def update(part):
+
+    try:
+        runcommand("bumpversion {} --no-commit --no-tag".format(part), check=True)
+    except:
+        return
+    
+    runcommand("autoreconf -fiv")
+
+    runcommand("git add Makefile.in config.h.in~ configure")
+    
+    runcommand("git checkout configure.ac .bumpversion.cfg")
+
+    runcommand("git commit -m \"Automatically autoreconf'ed\"")
+
+    runcommand("bumpversion {}".format(part))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Manage bumpversion/autotools interface")
+    parser.add_argument('part', nargs=1, choices=['major', 'minor', 'patch'])
+
+    args = parser.parse_args()
+    part = args.part[0]
+
+    print(part)
+    update(part)
