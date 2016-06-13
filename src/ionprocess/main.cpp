6f0b1d28e6e8813f1b15deb3944109b35cadf840
Joel Frederico <joelfred@slac.stanford.edu>
2016-06-13 11:15:25 -0700

    Ionprocess library updates for python compatibility

--- src/ionprocess/main.cpp
+++ src/ionprocess/main.cpp
@@ -1,35 +1,27 @@
-#include "config.h"
+/* #include "config.h" */
 #include "main.h"
-#include "../hdf5_classes.h"
 #include <gflags/gflags.h> // #include <google/gflags.h>
+#include <ionprocess.h>
+#include "../hdf5_classes.h"
 
 DEFINE_bool(verbose, true, "Verbose");
 DEFINE_int32(step, 0, "Electron Step");
+DEFINE_int32(xbins, 101, "Number of bins in x");
 /* DEFINE_string(file, "config.h5", "Data file"); */
 
 
-std::string _getion(unsigned int step)
-{
-	std::string out;
-	char buf[10];
-	sprintf(buf, "ions_%04u", step);
-	out = buf;
-	return out;
-}
 
 int main(int argc, char **argv)
 {
-	// ==============================
-	// Initialize Vars 
-	// ==============================
-	std::string filename;
-	std::string basename;
-	std::string procname;
+	std::string filename, basename, procname;
+	hsize_t dims[2];
+	hsize_t memdims[1];
 
 	// ==============================
 	// Parse flags
 	// ==============================
 	gflags::ParseCommandLineFlags(&argc, &argv, true);
+	int xbins = FLAGS_xbins;
 
 	// ==============================
 	// Get filename
@@ -48,26 +40,30 @@ int main(int argc, char **argv)
 	basename = filename.substr(0, filename.find_last_of("."));
 	procname = basename + "_processed.h5";
 
+	unsigned long long *hist;
+	long histsize;
+	int n_field_z;
+
+	makehist(filename, xbins, 0, hist, histsize, n_field_z);
+	/* std::cout << "Hist[0]: " << hist[0] << std::endl; */
+	/* std::cout << "Hist: " << hist << std::endl; */
+	/* std::cout << "histsize: " << histsize << std::endl; */
+	/* std::cout << "n_field_z: " << n_field_z << std::endl; */
+
 	// ==============================
-	// Open files for reading/writing
+	// Write to file
 	// ==============================
-	FileOpen data(filename);
+	DataspaceCreate *dataspace, *memspace;
 	FileCreate output(procname);
+	dims[0] = xbins;
+	dims[1] = n_field_z;
+	memdims[0] = xbins*n_field_z;
+	DatasetAccess hist_data(output.file_id, "hist", 2, dims);
+	memspace = new DataspaceCreate(1, memdims);
 
-	// ==============================
-	// Find ions and number of them
-	// ==============================
-	GroupStepAccess step(data.file_id, FLAGS_step);
-	GroupAccess ionstep(step.group_id, "ions_steps");
-	AttributeOpen n_ions(data.file_id, "n_field_z");
+	H5Dwrite(hist_data.dataset_id, H5T_NATIVE_ULLONG, memspace->dataspace_id, hist_data.dataspace_id, H5P_DEFAULT, hist);
 
-	std::cout << "N_ions: " << n_ions.read() << std::endl;
-	
-	// ==============================
-	// Loop over ions
-	// ==============================
-	DatasetOpen *ions;
-	ions = new DatasetOpen(ionstep.group_id, _getion(0));
-	delete ions;
-
+	delete memspace;
+	delete[] hist;
+	return 0;
 }
